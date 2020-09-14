52800bf8c3d1ed4cb52b7359cc51105397c53492
Trujillo <heber.trj.urt@gmail.com>
2020-09-14 14:27:05 +0200

    EDA functions and TCC added

--- utilities/functions.py
+++ utilities/functions.py
@@ -81,3 +81,91 @@ def get_cat_summary(df):
     cat_summary_df.columns = ['ColName', 'BiggestCat', 'NumLevels', 'BiggestShare']
     
     return cat_summary_df
+
+
+def plot_churn_probs(df_z_test):
+    '''
+    plot the churn with their confidence intervals and the total number of observations
+
+    Parameters
+    ----------
+    df_z_test : pd.DataFrame
+        DataFrame containing:
+            + categories
+            + p_churn: churn probability for each category.
+            + churn: churn observations  for each category.
+            + count: total number of observation for each category.
+            + ci_(low/upp): lower and upper boundaries for the churn probability according to their confidence interval.
+
+    Returns
+    -------
+    None.
+
+    '''
+    
+    import plotly.graph_objects as go
+    from plotly.subplots import make_subplots
+    column = df_z_test.columns[0]
+    
+    fig = make_subplots(specs=[[{"secondary_y": True}]])
+    fig.add_trace(
+        go.Scatter(x=df_z_test[column], y=df_z_test['ci_upp'], name="Upper Boundary", marker=dict(color='#6E7B8B')),
+        secondary_y=True)
+    fig.add_trace(
+        go.Scatter(x=df_z_test[column], y=df_z_test['p_churn'], name="Churn Prob", marker=dict(color='#009ACD')),
+        secondary_y=True)
+    fig.add_trace(
+        go.Scatter(x=df_z_test[column], y=df_z_test['ci_low'], name="Lower Boundary", marker=dict(color='#6E7B8B')),
+        secondary_y=True)
+
+    fig.add_trace(
+        go.Bar(x=df_z_test[column], y=df_z_test['count'], name="No. Obs", marker=dict(color='#FFC125')), secondary_y=False)
+    fig.update_xaxes(title_text=column)
+    
+    fig.update_yaxes(title_text="No. obs", secondary_y=False)
+    fig.update_yaxes(title_text="Probability", secondary_y=True)
+
+    fig.show()
+    
+
+def compare_churn_prob(df, column, churn = 'Churn', plot = True):
+    '''
+    Calculates the churn probability and the confidence interval for each category.
+
+    Parameters
+    ----------
+    df : pd.DataFrame
+        The Pandas DataFrame to be analyzed .
+    column : srt
+        Column name containing the categories to be compared.
+    churn : str, optional
+        Churn column. The default is 'Churn'.
+
+    plot : bool, optional
+        if true, the plot_churn_probs will be executed.
+
+    Returns
+    -------
+    df_z_test : pd.DataFrame
+        DataFrame containing:
+            + categories
+            + p_churn: churn probability for each category.
+            + churn: churn observations  for each category.
+            + count: total number of observation for each category.
+            + ci_(low/upp): lower and upper boundaries for the churn probability according to their confidence interval.
+
+    '''
+    from statsmodels.stats.proportion import proportion_confint
+    df_z_test = df.groupby(column).agg(p_churn = (churn, 'mean'), churn = (churn, 'sum'), count = (churn, 'count')).reset_index()
+
+    ci_low, ci_upp = proportion_confint(count = df_z_test['churn'], nobs = df_z_test['count'])
+
+    df_z_test['ci_low'] = ci_low
+    df_z_test['ci_upp'] = ci_upp
+    
+    if plot:
+        plot_churn_probs(df_z_test)    
+    
+    return df_z_test
+
+
